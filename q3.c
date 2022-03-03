5abbb9f85074bfc002a8991e5c307bfbbb28ed09
Anmol Narang <nanmol0406@gmail.com>
2022-03-03 17:44:47 +0530

    Questions 1-3

---
+++ q3.c
@@ -0,0 +1,44 @@
+//program to print the minimum value at the kth value
+#include <stdio.h>
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// A function to implement bubble sort
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)      
+
+       // Last i elements are already in place   
+       for (j = 0; j < n-i-1; j++) 
+           if (arr[j] > arr[j+1])
+              swap(&arr[j], &arr[j+1]);
+}
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i,k;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+    printf("Enter the value of the kth element\n");
+    scanf("%d",&k); 
+    printf("The minimum number at the kth value is %d\n" , arr[k-1]);
+
+}
+
+// Driver program to test above functions
+int main()
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
